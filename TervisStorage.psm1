3d19b670b715ab1332b208cdefbb12bf80ce90a7
mastermohl <mastermohl@gmail.com>
2017-08-14 11:44:13 -0400

    Template Fix

--- TervisStorage.psm1
+++ TervisStorage.psm1
@@ -234,7 +234,7 @@ function Get-StorageGroupsFromVNX{
         $TervisStorageArrayPasswordDetails = Get-PasswordstateEntryDetails -PasswordID $TervisStorageArrayDetails.PasswordstateCredentialID
         $RawStorageGroupOutput = & 'c:\program files\emc\naviseccli.exe' -scope 0 -h $TervisStorageArrayDetails.IPAddress -user $TervisStorageArrayPasswordDetails.Username -password $TervisStorageArrayPasswordDetails.Password storagegroup -list
         $output = $RawStorageGroupOutput | ConvertFrom-String -TemplateFile $PSScriptRoot\VNX_StorageGroup_Template.txt
-        $LunsFromVNX = Get-LUNSFromVNX -TervisStorageArraySelection $TervisStorageArraySelection
+        $LunsFromVNX = Get-LUNSFromVNX -TervisStorageArraySelection $Array
         Foreach ($Storagegroup in $output){
             $StorageGroupName = $StorageGroup.StorageGroupName
             if(-not $StorageGroup.LUNS){
@@ -253,7 +253,7 @@ function Get-StorageGroupsFromVNX{
                     HLUNumber = $_.HLUNumber
                     ALUNumber = $_.ALUNumber
                     Name = $LUNDetail.LUNNAME
-                    "Capacity GB" = ($LUNDetail.LUNCapacity / 1KB)
+                    "Capacity GB" = (($LUNDetail.LUNCapacity) / 1KB)
                     LUNUID = $LUNDetail.LUNUID
                 }
             }
